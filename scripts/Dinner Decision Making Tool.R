b6b893ee79359c613a0e11428be438723c22ab87
cjwheatley88 <cjwheatley88@gmail.com>
2022-01-16 13:33:40 +1100

    formatting for meals, updated ingredients, labelled histogram, started linking ingredients to meals in matrix 'm'.

--- scripts/Dinner Decision Making Tool.R
+++ scripts/Dinner Decision Making Tool.R
@@ -4,30 +4,36 @@ library(tidyverse)
 meal_database <- c("Steak, Chips and Salad", "Homemade Pizza", "Spaghetti Bologna", 
             "Butter Chicken", "Sweet Potato Pockets", "Lamb Backstrap Salad",
             "Mexican Bowls", "Pesto Pasta", "Lamb Chops, Mash and Veggies",
-            "chilli and cornbread", "risotto", "Poke Bowls", 
+            "Chilli and Cornbread", "Risotto", "Poke Bowls", 
             "Reheat Meal", "Homemade Burgers", "One Pot Chicken","Chicken Schnitzel",
             "Bacon and Blue Cheese Pie")
+
+takeout <- "Takeout"
+
+new_recipe <- "New Recipe"
 
 #Frequency of meals set by probabilities.
 
 probabilities <- c((rep(1/14,times=length(meal_database))),1/7, 1/28)
 
-meal_data <- data.frame(dinners = c(meal_database, takeout, new_recipe), probs = probabilities) %>%
-  mutate(index = 1:length(dinners))
+meal_data <- data.frame(meals = c(meal_database, takeout, new_recipe),
+                        probability = probabilities) %>%
+                        mutate(index = 1:length(meals))
 
 dinner_schedule <- sample(meal_data$index, 7, replace = FALSE, prob = probabilities)
 
 dinner_schedule #sampled index list
 
-meal_data$dinners[dinner_schedule] #converted to meal list
+meal_data$meals[dinner_schedule] #converted to list of meals
 
-# Testing probability distribution.
+# test and plot probability distribution.
 
-test <- data.frame(meals = sample(meal_data$dinners, 10000, replace = TRUE, prob = probabilities))
+test <- data.frame(meals = sample(meal_data$meals, 10000, replace = TRUE, prob = probabilities))
 
 test %>% ggplot() +
   geom_histogram(aes(meals), stat = "count") +
-  theme(axis.text.x = element_text(angle = -90))
+  theme(axis.text.x = element_text(angle = -90)) +
+  ggtitle("Meal Sampling Distribution")
 
 #Intuitively feels okay.
 
@@ -38,15 +44,24 @@ ingredients <- c("avocado", "asparagus", "bread_gluten_free", "bread", "bacon","
                  "oil_cooking", "pasta", "pizza_bases", "pesto", "rocket", "chorizo", "salami",
                  "cauliflower", "chicken_breast", "chicken_thigh", "chicken_schnitzel",
                  "proscuitto", "pumpkin", "sour_cream", "lamb_chops", "pickled_ginger", 
-                 "dried_shallots", "cucumber", "baby_cos_lettuce")
+                 "dried_shallots", "cucumber", "baby_cos_lettuce","steak","tomatoes",
+                 "oven_chips","pizza_bases","cheese_mozzarella", "tomato_paste","pepperoni")
 
-#matrix - row names trialed as meal names or index for retrieval. Each meal will assign a 1 to it's req'd ingredient.
+#Build matrix with m x n dimensions, m/rows = ingredients and n/columns = meals.
 
-m <- matrix(nrow = length(ingredients), ncol = length(meal_data$dinners))
+m <- matrix(nrow = length(ingredients), ncol = length(meal_data$meals))
 
-colnames(m) <- c(meal_data$dinners)
+colnames(m) <- c(meal_data$meals)
 
 row.names(m) <- c(ingredients)
 
-#link ingredients to meals; to assist in pulling a grocery list.
+#This is the painful part... link ingredients to meals; to assist in pulling a grocery list.
+#Assigning a binary indicator = 1; to relevant ingredients with each meal.
+
+#steak chips and salad
+m[c("rocket","steak","tomatoes","onion_red","oven_chips","avocado"),"Steak, Chips and Salad"] <- 1
+
+#Homemade Pizza
+m[c("pizza_bases","pumpkin","olives","cheese_mozzarella","tomato_paste","pepperoni"),"Homemade Pizza"] <- 1
+
 
